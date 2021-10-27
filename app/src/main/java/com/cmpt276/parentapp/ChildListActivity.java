80e46460adbd8092d25d48f409e203beea04b3b5
Sagardeep Singh Hunjan <ssh10@sfu.ca>
2021-10-27 08:13:27 -0700

    Updated buttons to use the static get Intent Methods from activity classes.

--- app/src/main/java/com/cmpt276/parentapp/ChildListActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/ChildListActivity.java
@@ -1,10 +1,16 @@
 package com.cmpt276.parentapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class ChildListActivity extends AppCompatActivity {
+
+    public static Intent getIntent(Context context) {
+       return new Intent(context, ChildListActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/cmpt276/parentapp/MainActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/MainActivity.java
@@ -1,25 +1,11 @@
 package com.cmpt276.parentapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.view.Menu;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
 import androidx.appcompat.widget.Toolbar;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.cmpt276.parentapp.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,27 +24,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupChildButton() {
-        Button child = (Button) findViewById(R.id.start_child_list);
-        child.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, ChildListActivity.class);
-            startActivity(intent);
-        });
+        Button child = findViewById(R.id.start_child_list);
+        child.setOnClickListener(view -> startActivity(ChildListActivity.getIntent(this)));
     }
 
     private void setupTimerButton() {
-        Button timer = (Button) findViewById(R.id.start_timer);
-        timer.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, TimerActivity.class);
-            startActivity(intent);
-        });
+        Button timer = findViewById(R.id.start_timer);
+        timer.setOnClickListener(view -> startActivity(TimerActivity.getIntent(this)));
     }
 
     private void setupFlipButton() {
-        Button flip = (Button) findViewById(R.id.start_flip);
-        flip.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, FlipActivity.class);
-            startActivity(intent);
-        });
+        Button flip = findViewById(R.id.start_flip);
+        flip.setOnClickListener(view -> startActivity(FlipActivity.getIntent(this)));
     }
 
     @Override
