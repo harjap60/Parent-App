ababc34c27090d122cb2f8baaf3fec3509544b79
sukhpalgosal <sukhpal_gosal@hotmail.com>
2021-10-27 17:24:53 -0700

    Update FlipActivity Update activity_flip.xml Add heads image of coin Add tails image of coin
    The FlipActivity will flip the coin with an onClickListener.
    When the coin is clicked, there will be an animation,
    currently the coin starts to heads and always ends on tails.
    
    The activity_flip.xml has been updated to include an image view
    for the coin, initially it is set to heads.

--- app/src/main/java/com/cmpt276/parentapp/FlipActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/FlipActivity.java
@@ -2,7 +2,14 @@ package com.cmpt276.parentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
 
 public class FlipActivity extends AppCompatActivity {
 
@@ -10,5 +17,35 @@ public class FlipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip);
+
+        flipCoin();
+    }
+
+    private void flipCoin(){
+        ImageView imageView = findViewById(R.id.coin_image_view);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ObjectAnimator firstAnimation = ObjectAnimator
+                        .ofFloat(imageView, "scaleY", 1f, 0f);
+                final ObjectAnimator secondAnimation = ObjectAnimator
+                        .ofFloat(imageView, "scaleY", 0f, 1f);
+
+                firstAnimation.setDuration(100);
+                firstAnimation.setRepeatCount(100);
+                firstAnimation.setInterpolator(new DecelerateInterpolator());
+                secondAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
+
+                firstAnimation.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                        imageView.setImageResource(R.drawable.tails);
+                        secondAnimation.start();
+                    }
+                });
+                firstAnimation.start();
+            }
+        });
     }
 }
