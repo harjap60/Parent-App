25b550ba27354394f23ede98f0aadbaad1bee0f7
sukhpalgosal <sukhpal_gosal@hotmail.com>
2021-10-29 11:28:07 -0700

    Add noise for coin Add random side of coin after flip
    When a coin is flipped a noise is played.
    
    When the coin is flipped and random side of the coin is chosen to be displayed.

--- app/src/main/java/com/cmpt276/parentapp/FlipActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/FlipActivity.java
@@ -5,47 +5,62 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class FlipActivity extends AppCompatActivity {
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip);
-
+        random = new Random();
         flipCoin();
     }
 
-    private void flipCoin(){
+    private void flipCoin() {
         ImageView imageView = findViewById(R.id.coin_image_view);
+        final MediaPlayer coinFlip = MediaPlayer.create(this, R.raw.coinflip);
+        //Source: https://stackoverflow.com/questions/46111262/card-flip-animation-in-android
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final ObjectAnimator firstAnimation = ObjectAnimator
-                        .ofFloat(imageView, "scaleY", 1f, 0f);
-                final ObjectAnimator secondAnimation = ObjectAnimator
-                        .ofFloat(imageView, "scaleY", 0f, 1f);
-
-                firstAnimation.setDuration(100);
+                coinFlip.start();
+                final ObjectAnimator firstAnimation = ObjectAnimator.ofFloat(imageView, "scaleY", 1f, 0f);
+                final ObjectAnimator secondAnimation = ObjectAnimator.ofFloat(imageView, "scaleY", 0f, 1f);
+                //50 = 2.5 seconds
+                firstAnimation.setDuration(50);
+                //Number of flips the coin does in the duration
                 firstAnimation.setRepeatCount(100);
                 firstAnimation.setInterpolator(new DecelerateInterpolator());
                 secondAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
-
                 firstAnimation.addListener(new AnimatorListenerAdapter() {
                     @Override
                     public void onAnimationEnd(Animator animation) {
                         super.onAnimationEnd(animation);
-                        imageView.setImageResource(R.drawable.tails);
+                        imageView.setImageResource(determineSide());
                         secondAnimation.start();
                     }
                 });
                 firstAnimation.start();
             }
         });
+    }
+
+    private int determineSide() {
+        int randomNum = random.nextInt(2);
+        if (randomNum == 1) {
+            return R.drawable.heads;
+        } else {
+            return R.drawable.tails;
+        }
+
     }
 }
