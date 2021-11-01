1f260846419514d3eb071154ff11b0239ebcb3a7
Sagardeep Singh Hunjan <ssh10@sfu.ca>
2021-10-31 22:41:31 -0700

    Moved timer to function scope. Added button to cancel timer.

--- app/src/main/java/com/cmpt276/parentapp/TimerActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/TimerActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +16,6 @@ public class TimerActivity extends AppCompatActivity {
     public static final int COUNT_DOWN_INTERVAL = 1000;
     public static final int SECONDS_IN_MINUTE = 60;
     public static final String TIMER_DURATION = "TIMER_DURATION_TAG";
-    private CountDownTimer timer = null;
 
     public static Intent getIntentWithDurationMinutes(Context context, int duration) {
         Intent i = new Intent(context, TimerActivity.class);
@@ -34,7 +34,7 @@ public class TimerActivity extends AppCompatActivity {
     private void setupTimer() {
         long duration = this.getIntent().getIntExtra(TIMER_DURATION, 0);
         TextView tvTimerRemaining = this.findViewById(R.id.tvTimeRemaining);
-        timer = new CountDownTimer(duration * COUNT_DOWN_INTERVAL, COUNT_DOWN_INTERVAL) {
+        CountDownTimer timer = new CountDownTimer(duration * COUNT_DOWN_INTERVAL, COUNT_DOWN_INTERVAL) {
             @Override
             public void onTick(long millisUntilFinished) {
                 long totalSeconds = millisUntilFinished / COUNT_DOWN_INTERVAL;
@@ -49,8 +49,15 @@ public class TimerActivity extends AppCompatActivity {
             public void onFinish() {
                 tvTimerRemaining.setText(R.string.timer_activity_end_message);
             }
-
         };
+
+        Button btnTimerCancel = this.findViewById(R.id.btnTimerCancel);
+
+        btnTimerCancel.setOnClickListener(v -> {
+            timer.cancel();
+            finish();
+        });
+
         timer.start();
     }
 }
