eec12ee4339cc4bf93c4b2df1333f0abcc2f00fe
harjapdhaliwal <hda36@sfu.ca>
2021-10-31 22:07:44 -0700

    Adding pop-up translucent options activity for Timer. Design still needed

---
+++ app/src/main/java/com/cmpt276/parentapp/TimerOptionsActivity.java
@@ -0,0 +1,15 @@
+package com.cmpt276.parentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TimerOptionsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer_options);
+    }
+}
