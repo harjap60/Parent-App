739c60b9af9c02660871c3712cc0839858d48c93
Sagardeep Singh Hunjan <ssh10@sfu.ca>
2021-11-12 00:27:38 -0800

    Update ChildDao to use rxjava module for asynchronous data accesses.

--- app/src/main/java/com/cmpt276/parentapp/model/ChildDao.java
+++ app/src/main/java/com/cmpt276/parentapp/model/ChildDao.java
@@ -1,24 +1,37 @@
 package com.cmpt276.parentapp.model;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+
+@Dao
 public interface ChildDao {
     @Query("SELECT * FROM child")
-    List<Child> getAll();
+    Single<List<Child>> getAll();
+
+    @Query("SELECT * FROM child WHERE uid = :userid")
+    Single<Child> get(int userid);
 
     @Query("SELECT * FROM child WHERE uid IN (:userIds)")
-    List<Child> loadAllByIds(int[] userIds);
+    Single<List<Child>> loadAllByIds(int[] userIds);
 
     @Query("SELECT * FROM Child WHERE name LIKE :name")
-    List<Child> findByName(String name);
+    Single<List<Child>> findByName(String name);
 
-    @Insert
-    void insertAll(Child... users);
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Completable insertAll(Child... children);
 
     @Delete
-    void delete(Child user);
+    Completable delete(Child child);
+
+    @Update
+    Completable update(Child child);
 }
