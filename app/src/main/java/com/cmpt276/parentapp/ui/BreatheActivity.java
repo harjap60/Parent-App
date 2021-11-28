59adf87756c76e997da7f55f22bc22efb7c0c9a7
sukhpalgosal <sukhpal_gosal@hotmail.com>
2021-11-27 17:26:13 -0800

    Button size reset if held less than 3 seconds

--- app/src/main/java/com/cmpt276/parentapp/ui/BreatheActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/ui/BreatheActivity.java
@@ -33,6 +33,9 @@ import java.util.concurrent.TimeUnit;
  */
 public class BreatheActivity extends AppCompatActivity {
 
+    public static final float BUTTON_SIZE_MAX = 2f;
+    public static final int MAX_ANIMATION_DURATION = 10000;
+    public static final int TIME_BREATHE_GOOD = 3;
     private ActivityBreatheBinding binding;
     private long lastDown;
     private long lastDuration;
@@ -75,12 +78,11 @@ public class BreatheActivity extends AppCompatActivity {
                 binding.breatheButton.setBackgroundColor(Color.BLACK);
 
                 //Animation for button size increase
-                ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(binding.breatheButton, "scaleX", 2f);
-                ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(binding.breatheButton, "scaleY", 2f);
-                scaleDownX.setDuration(10000);
-                scaleDownY.setDuration(10000);
+                ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(binding.breatheButton, "scaleX", BUTTON_SIZE_MAX);
+                ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(binding.breatheButton, "scaleY", BUTTON_SIZE_MAX);
+                scaleDownX.setDuration(MAX_ANIMATION_DURATION);
+                scaleDownY.setDuration(MAX_ANIMATION_DURATION);
                 scaleUp.play(scaleDownX).with(scaleDownY);
-                scaleUp.cancel();
                 scaleUp.start();
 
                 //Tell user to let go of button after 10s
@@ -90,13 +92,14 @@ public class BreatheActivity extends AppCompatActivity {
                                 "LET GO OF ME!!!",
                                 Toast.LENGTH_SHORT)
                                 .show(),
-                        10000);
+                        MAX_ANIMATION_DURATION);
 
             } else if(motionEvent.getAction() == MotionEvent.ACTION_UP){
                 scaleUp.pause();
                 lastDuration = System.currentTimeMillis() - lastDown;
-                if(TimeUnit.MILLISECONDS.toSeconds(lastDuration) < 3){
+                if(TimeUnit.MILLISECONDS.toSeconds(lastDuration) < TIME_BREATHE_GOOD){
                     resetSize(binding.breatheButton);
+                    scaleUp.cancel();
                 }
                 binding.breatheButton.setBackgroundColor(ContextCompat.getColor(this, R.color.primaryVariant));
                 Toast.makeText(
