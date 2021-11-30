e6534322ea9f33c8fe88f9fc4788a1c56c98e072
sukhpalgosal <sukhpal_gosal@hotmail.com>
2021-11-30 14:00:37 -0800

    Update button size to decrease
    If button is held for 3 or more seconds, when the button is released it will
    start to shrink back to its normal size.

--- app/src/main/java/com/cmpt276/parentapp/ui/BreatheActivity.java
+++ app/src/main/java/com/cmpt276/parentapp/ui/BreatheActivity.java
@@ -15,7 +15,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.cmpt276.parentapp.R;
 import com.cmpt276.parentapp.databinding.ActivityBreatheBinding;
@@ -28,9 +27,6 @@ import java.util.concurrent.TimeUnit;
  * Current state:
  * Just has a textview and button
  * <p>
- * TODO:
- * Make button increase size the longer it is held
- * When released reset to normal size
  */
 public class BreatheActivity extends AppCompatActivity {
 
@@ -40,8 +36,8 @@ public class BreatheActivity extends AppCompatActivity {
     private final int MIN_NUM_BREATHS = 0;
     private final int MAX_NUM_BREATHS = 10;
     private ActivityBreatheBinding binding;
-    private long lastDown;
-    private long lastDuration;
+    private long buttonPressedTimerStart;
+    private long timeButtonHeldFor;
 
     private int numBreaths;
     int breathsTaken = 0;
@@ -148,15 +144,15 @@ public class BreatheActivity extends AppCompatActivity {
 
                 binding.breatheButton.setText(R.string.breathe_in_button_text);
                 if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    lastDown = System.currentTimeMillis();
+                    buttonPressedTimerStart = System.currentTimeMillis();
                     binding.breatheButton.setBackgroundColor(Color.BLACK);
 
                     //Animation for button size increase
-                    ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(binding.breatheButton, "scaleX", BUTTON_SIZE_MAX);
-                    ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(binding.breatheButton, "scaleY", BUTTON_SIZE_MAX);
-                    scaleDownX.setDuration(MAX_ANIMATION_DURATION);
-                    scaleDownY.setDuration(MAX_ANIMATION_DURATION);
-                    scaleUp.play(scaleDownX).with(scaleDownY);
+                    ObjectAnimator scaleUpX = ObjectAnimator.ofFloat(binding.breatheButton, "scaleX", BUTTON_SIZE_MAX);
+                    ObjectAnimator scaleUpY = ObjectAnimator.ofFloat(binding.breatheButton, "scaleY", BUTTON_SIZE_MAX);
+                    scaleUpX.setDuration(MAX_ANIMATION_DURATION);
+                    scaleUpY.setDuration(MAX_ANIMATION_DURATION);
+                    scaleUp.play(scaleUpX).with(scaleUpY);
                     scaleUp.start();
 
                     //Tell user to let go of button after 10s
@@ -169,42 +165,73 @@ public class BreatheActivity extends AppCompatActivity {
                             MAX_ANIMATION_DURATION);
 
                 } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+
                     handler.removeCallbacksAndMessages(null);
                     scaleUp.cancel();
-                    lastDuration = System.currentTimeMillis() - lastDown;
 
-                    if (TimeUnit.MILLISECONDS.toSeconds(lastDuration) >= TIME_BREATHE_GOOD) {
+
+                    timeButtonHeldFor = System.currentTimeMillis() - buttonPressedTimerStart;
+
+                    //Button held for more than 3 seconds
+                    if (TimeUnit.MILLISECONDS.toSeconds(timeButtonHeldFor) >= TIME_BREATHE_GOOD) {
                         binding.breatheButton.setText(R.string.breathe_out_button_text);
 
                         breathsTaken++;
                         updateBreathCountTextView();
 
-                        handler.postDelayed(() -> {
-                            // also 'might' want to disable the onTouchListener on the button for
-                            // the next 3 seconds while the user exhales out
+                        //Set button text
+                        if (breathsTaken == numBreaths) {
+                            binding.breatheButton.setText(R.string.all_breaths_taken);
+                        } else {
+                            binding.breatheButton.setText(R.string.breathe_in_button_text);
+                        }
 
-                            if (breathsTaken == numBreaths) {
-                                binding.breatheButton.setText(R.string.all_breaths_taken);
-                            } else {
-                                binding.breatheButton.setText(R.string.breathe_in_button_text);
-                            }
-                        }, 3000);
-                    }
+                        buttonExhale(binding.breatheButton.getScaleX(), binding.breatheButton.getScaleY());
 
-                    if (TimeUnit.MILLISECONDS.toSeconds(lastDuration) < TIME_BREATHE_GOOD) {
+
+                        /*** Not sure what this is for thats why i commented it out ***/
+//                        handler.postDelayed(() -> {
+//                            // also 'might' want to disable the onTouchListener on the button for
+//                            // the next 3 seconds while the user exhales out
+//
+//                            if (breathsTaken == numBreaths) {
+//                                binding.breatheButton.setText(R.string.all_breaths_taken);
+//                            } else {
+//                                binding.breatheButton.setText(R.string.breathe_in_button_text);
+//                            }
+//                        }, 3000);
+
+
+                    } else {
                         resetSize(binding.breatheButton);
-                        scaleUp.cancel();
                     }
-                    binding.breatheButton.setBackgroundColor(ContextCompat.getColor(this, R.color.primaryVariant));
                     Toast.makeText(
                             this,
-                            getResources().getString(R.string.button_time_held, TimeUnit.MILLISECONDS.toSeconds(lastDuration)),
+                            getResources().getString(R.string.button_time_held, TimeUnit.MILLISECONDS.toSeconds(timeButtonHeldFor)),
                             Toast.LENGTH_SHORT).show();
                 }
 
             }
             return true;
         });
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void buttonExhale(float buttonX, float buttonY) {
+        AnimatorSet scaleDown = new AnimatorSet();
+
+        binding.breatheButton.setScaleX(buttonX);
+        binding.breatheButton.setScaleY(buttonY);
+        binding.breatheButton.setBackgroundColor(Color.RED);
+
+        //Animation for button size increase
+        ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(binding.breatheButton, "scaleX", 1f);
+        ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(binding.breatheButton, "scaleY", 1f);
+        scaleDownX.setDuration(MAX_ANIMATION_DURATION);
+        scaleDownY.setDuration(MAX_ANIMATION_DURATION);
+        scaleDown.play(scaleDownX).with(scaleDownY);
+        scaleDown.start();
+
     }
 
     private void resetSize(Button btn) {
